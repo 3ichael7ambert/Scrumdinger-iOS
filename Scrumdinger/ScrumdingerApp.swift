8d3f5cf3f1f4ffec0bdb7ac4c6828e3ffa137d76
3ichael7ambert <mlambertbusiness@gmail.com>
2024-04-04 22:07:59 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 3ichael 7ambert on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
